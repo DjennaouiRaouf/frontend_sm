0f6737167c49c643c343aa4860c5724b1a8adbeb
djennaoui <djennaoui.raouf@gmail.com>
2024-02-12 10:44:31 +0100

    Initial commit

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,101 @@
 import * as React from "react";
-
+import {Carousel} from "react-bootstrap";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import "./Home.css";
 type HomeProps = {
     //
 };
 
 const Home: React.FC<any> = () => {
-    return <div>Home</div>;
+    const [pics,setPics]=useState<any[]>([]);
+    const [timeline,setTimeline]=useState<any[]>([]);
+    const getImages = async () => {
+        await axios.get(`${process.env.REACT_APP_API_BASE_URL}/sm/ic_images/?type=H`,{
+            headers:{
+                "Content-Type":"application/json",
+            }
+        })
+            .then((response) => {
+                setPics(response.data);
+
+            })
+            .catch((error) => {
+
+            });
+
+    }
+    const getTimeline = async () => {
+        await axios.get(`${process.env.REACT_APP_API_BASE_URL}/sm/timeline/`,{
+            headers:{
+                "Content-Type":"application/json",
+            }
+        })
+            .then((response) => {
+                setTimeline(response.data);
+
+            })
+            .catch((error) => {
+
+            });
+
+    }
+    useEffect(() => {
+        getImages();
+
+
+    },[]);
+    useEffect(() => {
+        getTimeline();
+
+
+    },[]);
+    return <>
+        <Carousel className="w-100 d-block mb-5" controls={false} interval={1000} fade={true} indicators={true} style={{borderWidth: "1px",borderRadius: "8px"}}>
+            {pics.map((item,index) => (
+                <Carousel.Item key={index}  style={{borderWidth: "1px",borderRadius: "8px"}}>
+                    <img
+                        src={item.src}
+                        alt={""}
+                        height={500}
+                        className="d-block w-100"
+                        style={{borderWidth: "1px",borderRadius: "8px"}}
+
+                    />
+
+                </Carousel.Item>
+            ))}
+        </Carousel>
+        {
+            timeline.length > 0 &&
+            <div className="container">
+                <h3 className="card-title mb-5">Nos Marchés </h3>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="main-timeline2">
+                            {timeline.map((item,index) => (
+                            <div className="timeline">
+                                <a href="#" className="timeline-content">
+                                    <span className="year">{item.year}</span>
+                                    <div className="inner-content">
+                                        <h3 className="title">{item.title}</h3>
+                                        <p className="description">
+                                            {item.description}
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        }
+
+
+    </>;
 };
 
 export default Home;
